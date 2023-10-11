aed51518dd3160544f30684f9550617257e00265
ayam04 <ayamullahkhan04@gmail.com>
2023-10-11 17:19:22 +0530

    final code

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import streamlit as st
 import PyPDF2
-# Set the title and description of your app
-st.title("PDf Data extractor")
-extracted_text = ''
+from io import BytesIO
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+st.title("PDF Data Extractor")
+
 # File upload widget
 uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
+submitted = st.button("Submit")
 
-if uploaded_file is not None:
-    pdf_file=open(uploaded_file,'rb')
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-    for page_num in range(pdf_reader.numPages):
-        page = pdf_reader.getPage(page_num)
-        extracted_text += page.extractText()
-    pdf_file.close()
-    st.write(extracted_text)
+if submitted:
+    if uploaded_file is not None:
+            file = BytesIO(uploaded_file.read())
+            reader = PyPDF2.PdfReader(file)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+            words = nltk.word_tokenize(text)
+            words = [word.lower() for word in words]
+            stop_words = set(stopwords.words("english"))
+            words = [word for word in words if word not in stop_words]
+            lemmatizer = WordNetLemmatizer()
+            words = [lemmatizer.lemmatize(word) for word in words]
+            final_text = " ".join(words)
+            st.write(final_text)
+
 
